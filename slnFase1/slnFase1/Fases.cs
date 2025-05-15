7b6df3ede0bbe76f85cb630353b93daf28bed7a3
rmuñoz <rmuoz@SALESRMUNOZ>
2025-05-15 19:39:42 +0200

    SDolución semana 0

---
+++ slnFase1/slnFase1/Fases.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slnFase1
+{
+    public class Fases
+    {
+        public int numeroFase { get; private set; }
+        public bool completado { get; set; }
+        public int puntuacion { get; set; }
+
+        int pvp = 2;
+        int precio2 = 3;
+        decimal resultado = 0;
+        decimal impuesto = 0;
+        bool Aaceptado = false;
+
+
+        public Fases()
+        {
+
+            completado = false;
+            puntuacion = 0;
+        }
+
+        public Fases(int noFase)
+        {
+            numeroFase = noFase;
+
+        }
+
+        public bool ValidarCookies()
+        {
+            if (Aaceptado)
+            {
+                IMprimeCOnsentimiento()
+                ValidaLaVEnta()
+                EnviaEmail()
+            }
+            else
+            {
+
+            }
+        }
+
+        public void IMprimeCOnsentimiento()
+        {
+
+        }
+
+
+        public decimal CalcularElIVA(decimal importe, decimal impuestoAAplicar)
+        {
+            return (importe * impuestoAAplicar / 100) + importe;
+        }
+
+        public void CalcularElIVA(decimal importe, decimal impuestoAAplicar)
+        {
+            return   resultado= (importe * impuestoAAplicar / 100) + importe;
+
+         
+        }
+
+
+        public string EjecutarLaFase()
+        {
+            Console.WriteLine("La fase ha inciado");
+
+            AccionesAReaqlizar();
+
+            return "Finzalizado";
+        }
+
+
+        private void AccionesAReaqlizar()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine($"Estoy en la interacción {i}");
+            }
+        }
+    }
+}
---
+++ slnFase1/slnFase1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slnFase1
+{
+    class Program
+    {
+
+       
+        static void Main(string[] args)
+        {
+            foreach (string a in args)
+            {
+
+                if (a.ToLower() == "saludo")
+                {
+
+                    Fases gestorDeVenta = new Fases(4);
+
+                    gestorDeVenta.EjecutarLaFase();
+
+
+                                      
+
+                }
+                else if (a.ToLower() == "despedida")
+                {
+                   
+                }
+                else
+                {
+                   
+                }
+            }
+
+        }
+
+    }    
+
+}
